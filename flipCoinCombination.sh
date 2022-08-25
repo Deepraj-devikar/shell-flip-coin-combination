8cff2ca463ae56afc6ab23f1e30d23226045880f
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-25 12:06:14 +0530

    REFACTOR: replace heads or tails with singlet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
-echo "Welcome To Flip Coin Combination"
-
-function headsOrTails () {
-	echo $((RANDOM%2))
+function division () {
+	local result=`echo "$1/$2" | awk -F "/" "{print ($1/$2)}"`
+	echo $result
 }
+
+function multiplication () {
+	local result=`echo "$1*$2" | awk -F "*" "{print ($1*$2)}"`
+	echo $result
+}
+
+echo "Welcome To Flip Coin Combination"
 
 HEADS=0
 TAILS=1
 
-flipCoin=$(headsOrTails)
-if [ $flipCoin -eq $HEADS ]
-then
-	echo "Flip the coin and its heads"
-elif [ $flipCoin -eq $TAILS ]
-then
-	echo "Flip the coin and its tails"
-fi
+function headsOrTails () {
+	if [ $((RANDOM%2)) -eq $HEADS ]
+	then
+		echo "H"
+	else
+		echo "T"
+	fi
+}
+
+numberOfRounds=0 declare -A singlet
+singlet["H"]=0
+singlet["T"]=0
+
+for (( i=0; i<10; i++ ))
+do
+	flipCoin=$(headsOrTails)
+	singlet[$flipCoin]=$((${singlet[$flipCoin]}+1))
+	numberOfRounds=$(($numberOfRounds+1))
+done
+headsPercentage=$(division ${singlet["H"]} $numberOfRounds)
+headsPercentage=$(multiplication $headsPercentage 100)
+tailsPercentage=$(division ${singlet["T"]} $numberOfRounds)
+tailsPercentage=$(multiplication $tailsPercentage 100)
+echo "Percentage of heads is $headsPercentage"
+echo "Percentage of tails is $tailsPercentage"
+
+
