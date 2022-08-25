6928011ef72a37c27c4f13a31e6b8db7d85a91d0
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-08-25 11:42:45 +0530

    ADDED: Flip coin heads or tails

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
 echo "Welcome To Flip Coin Combination"
+
+function headsOrTails () {
+	echo $((RANDOM%2))
+}
+
+HEADS=0
+TAILS=1
+
+flipCoin=$(headsOrTails)
+if [ $flipCoin -eq $HEADS ]
+then
+	echo "Flip the coin and its heads"
+elif [ $flipCoin -eq $TAILS ]
+then
+	echo "Flip the coin and its tails"
+fi
